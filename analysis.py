b9bf30dcd61b356ec47fcb5b4060a5b34bb8dd35
lingqiz <zlqzcc@gmail.com>
2024-04-10 15:43:09 -0400

    add analysis

--- analysis.py
+++ analysis.py
@@ -10,8 +10,9 @@ def derivative(model, theta):
     with a finite difference implementation
     '''
     delta = 0.01
-    f_delta = model.orientation_response(theta + delta) - \
-              model.orientation_response(theta - delta)
+    with torch.no_grad():
+        f_delta = model.orientation_response(theta + delta) - \
+                  model.orientation_response(theta - delta)
     df_dtheta = f_delta / (2 * delta)
     return df_dtheta
 
@@ -63,3 +64,45 @@ class AlexNet(models.AlexNet):
 
         else:
             return self.feedforward(stimulus, self.layer_idx)
+
+class VGG16(models.vgg.VGG):
+    def __init__(self, stim_generator):
+        # init and load pre-trained VGG16 model
+        super(VGG16, self).__init__(models.vgg.make_layers(models.vgg.cfgs['D']))
+        self.generator = stim_generator
+
+    def feature_layer(self, x):
+        # access response of the feature layer
+        x = self.features(x)
+        x = x.view(-1, 512 * 7 * 7)
+        return x
+
+    def orientation_response(self, theta):
+        '''
+        Compute response to an orientation stimulus
+        '''
+        stimulus = self.generator(torch.tensor(theta))
+        return self.feature_layer(stimulus)
+
+class ViT(nn.Module):
+    def __init__(self, vit_model, stim_generator):
+        super(ViT, self).__init__()
+        self.vit = vit_model
+        self.generator = stim_generator
+
+    def feature_layer(self, x):
+        x = self.vit._process_input(x)
+        n = x.shape[0]
+
+        batch_class_token = self.vit.class_token.expand(n, -1, -1)
+        x = torch.cat([batch_class_token, x], dim=1)
+
+        x = self.vit.encoder(x)
+        return x[:, 0]
+
+    def orientation_response(self, theta):
+        '''
+        Compute response to an orientation stimulus
+        '''
+        stimulus = self.generator(torch.tensor(theta))
+        return self.feature_layer(stimulus)
